9fb41f5adae601fa8f02b5369bbb0ffa8d6c4bca
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-11-06 01:39:01 +0530

    blogs updated

---
+++ JS Assets/blogs.js
@@ -0,0 +1,14 @@
+var noOfclicks=0;
+function changeColor(x){
+  
+  x.classList.toggle('Like');
+    // noOfclicks++;
+    // if(noOfclicks%2!=0){
+    //     x.classList.toggle('Like');
+    // }
+    // else{
+    //     x.classList.add('disLike');
+    // }
+
+}
+
